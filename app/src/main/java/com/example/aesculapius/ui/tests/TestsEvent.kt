05a162080d54f2d6cbbc0173ff1e867374e22758
Maximus2004 <maks09062004@mail.ru>
2024-05-01 14:36:59 +0300

    Add TestsEvent

---
+++ app/src/main/java/com/example/aesculapius/ui/tests/TestsEvent.kt
@@ -0,0 +1,7 @@
+package com.example.aesculapius.ui.tests
+
+sealed interface TestsEvent {
+    data class OnInsertNewMetrics(val first: Float, val second: Float, val third: Float): TestsEvent
+    data class OnUpdateNewMetrics(val first: Float, val second: Float, val third: Float): TestsEvent
+    data class OnUpdateSummaryScore(val score: Int): TestsEvent
+}
